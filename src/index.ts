999d57257a2822f4d6e11321fc4c26af6c23a8ca
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-03-09 13:41:03 +0000

    rename static resource to public

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
 
 const app = new Hono()
+
+app.use('*', serveStatic({root: './public'}))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
