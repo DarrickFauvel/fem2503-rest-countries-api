1287935796a861d59e0a97a7b9c0c8506484e2fa
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-03-17 23:34:19 +0000

    cleanup home component

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { MoonIcon } from "@/views/components/icons/moon"
-import { SearchComponent } from "@/views/components/search"
+import { SearchControlComponent } from "@/views/components/search-control"
+import { SelectControlComponent } from "@/views/components/select-control"
 import { SpinnerIcon } from "@/views/components/icons/spinner"
 import { localStorageHeadScript } from "@/utils/localstorage-head-script"
 
@@ -11,10 +12,6 @@ export const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {localStorageHeadScript()}
         <script src="/public/js/htmx.min.js" defer></script>
-        <script
-          type="module"
-          src="https://cdn.jsdelivr.net/gh/starfederation/datastar@v1.0.0-beta.9/bundles/datastar.js"
-        ></script>
         <script src="/public/js/script.js" defer></script>
         <link rel="stylesheet" href="/public/css/output.css" />
         <title>REST Countries API</title>
@@ -33,27 +30,15 @@ export const Home = () => {
         </header>
 
         <main class="px-4 py-6 flex flex-col items-center gap-8">
-          {SearchComponent()}
-          <button
-            class="border bg-gray-200 px-4 py-2 cursor-pointer rounded-sm hover:bg-gray-300"
-            hx-get="/api/countries"
-            hx-target="#cards"
-          >
-            Get all countries
-          </button>
-          <button
-            hx-target="#cards"
-            hx-indicator="#spinner"
-            hx-on:click="clearCards()"
-          >
-            Clear cards
-          </button>
+          {SearchControlComponent()}
+
+          {SelectControlComponent()}
+
+          <section id="cards"></section>
 
           <div id="spinner" class="htmx-indicator">
             {SpinnerIcon()}
           </div>
-
-          <section id="cards"></section>
         </main>
       </body>
     </html>
