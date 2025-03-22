a31b0a7f2589679d2fc22ee773115720d5884e46
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-03-22 00:55:30 +0000

    make control clearable

--- src/views/components/select-control.tsx
+++ src/views/components/select-control.tsx
@@ -8,11 +8,13 @@ export const SelectControlComponent = () => {
   return (
     <select
       name="region"
+      id="select-control"
       class="bg-(--theme-light-fg) dark:bg-(--theme-dark-fg) px-6 py-3.5 rounded-[5px] self-start"
       hx-get="/api/countries"
       hx-target="#results"
       hx-trigger="revealed, change"
       hx-indicator="#spinner"
+      hx-on:change="clearSearchInput()"
     >
       <option value="">Filter By Region</option>
       {createRegionOptions()}
