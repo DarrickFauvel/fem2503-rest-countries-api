99157ee2d07f0f5a345d8b698370139a43246390
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-03-22 00:49:51 +0000

    add more country types

--- src/types/country.ts
+++ src/types/country.ts
@@ -1,7 +1,35 @@
 export type CountryProps = {
+  borders: string[]
+  capital: string
+  currencies: string[]
+  flag: string
+  languages: string[]
+  name: string
+  nativeName: string
+  population: string
+  region: string
+  subregion: string
+  topLevelDomain: string[]
+}
+
+export type CountrySummaryProps = {
+  capital: string
+  flag: string
   name: string
   population: string
   region: string
+  topLevelDomain: string
+}
+
+export type CountryDetailProps = {
+  borders: string[]
   capital: string
+  currencies: string[]
   flag: string
+  languages: string[]
+  nativeName: string
+  population: string
+  region: string
+  subregion: string
+  topLevelDomain: string[]
 }
