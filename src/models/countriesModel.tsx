e3a439abdc54155dd1fe5d72cc903591b0ccc8ee
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-04-06 08:16:51 +0000

    add country types

--- src/models/countriesModel.tsx
+++ src/models/countriesModel.tsx
@@ -1,22 +1,23 @@
+import { CountryProps } from "src/models/types/country"
 import { allCountriesArray } from "src/db"
 
 export const countriesCollection = allCountriesArray
 
 export const searchCountriesByName = async (query: string) => {
   const regex = new RegExp(query, "i")
-  return countriesCollection.filter((country) =>
+  return countriesCollection.filter((country: CountryProps) =>
     country.name.toLowerCase().includes(query)
   )
 }
 
 export const getCountriesByRegion = async (region: string) => {
-  return await countriesCollection.filter((country) =>
+  return await countriesCollection.filter((country: CountryProps) =>
     country.region.toLowerCase().includes(region.toLowerCase())
   )
 }
 
 export const getCountryByCode = async (code: string) => {
   return await countriesCollection.find(
-    (country) => country.alpha3Code.toLowerCase() === code.toLowerCase()
+    (country: CountryProps) => country.alpha3Code.toLowerCase() === code.toLowerCase()
   )
 }
