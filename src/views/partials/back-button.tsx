efd296c68705dc52158e0d4c278f2a19d2c6891c
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-04-06 14:56:34 +0000

    add styling and action

--- src/views/partials/back-button.tsx
+++ src/views/partials/back-button.tsx
@@ -1,3 +1,10 @@
 export const BackButton = () => {
-    return <button>&lt; Back</button>
+  return (
+    <button
+      class="bg-(--theme-light-fg) dark:bg-(--theme-dark-fg) px-6 py-3.5 rounded-[5px] cursor-pointer"
+      onclick="window.history.back()"
+    >
+      &lt; Back
+    </button>
+  )
 }
